c1c839f6c78148bf2933bddf570ccd4e7caec839
Gabriel Cavallo <gabrielcavallo@mail.com>
2016-07-27 00:25:10 -0400

    Add CT and CSV objects with open() method

--- asset/assets.py
+++ asset/assets.py
@@ -1,66 +1,40 @@
-import io, os, sys
+import os, sys
 
-class Asset(object):
+class CSV(object):
 
 	name = ""
 	header = []
 	types = []
 	rows = []
 
-	def __init__(self, asset):
-		"""Initialize object."""
+	def open(self, path):
+		"""
+		Parse CSV file to list
+
+		:param str path: Path to CSV file
+		:return: Parsed CSV data
+		:rtype: list
+		"""
+
 		try:
-			f = open(asset, "r", encoding="utf-16")
-			self.csv = io.StringIO(f.read())
-			f.close()
+			self.csv = open(path, "r", encoding="utf-16")
 		except (OSError, IOError):
-			print("could not open {0}".format(asset), file=sys.stderr)
+			print("could not open \"{0}\"".format(path), file=sys.stderr)
 
-		self.name = os.path.splitext(os.path.basename(asset))[0]
+		self.name = os.path.splitext(os.path.basename(path))[0]
+		self.header = self.csv.readline().rstrip("\n\r").split("\t")
+		self.types = self.csv.readline().rstrip("\n\r").split("\t")
+		self.rows = [line.rstrip("\n\r").split("\t") for line in self.csv]
 
-	def close(self):
-		"""Close CSV object to clean memory."""
 		self.csv.close()
+		return [self.header] + [self.types] + self.rows
 
-	def debug(self, full=False):
-		"""Print object attributes."""
-		print("Name:\n{0}\n".format(self.name))
-		print("Header:\n{0}\n".format(self.header))
-		print("Types:\n{0}\n".format(self.types))
-		if full:
-			print("Rows:\n{0}\n".format(self.rows))
-		else:
-			print("Row #1:\n{0}\n".format(self.rows[:1]))
+	def write(self, path, data):
+		"""
+		TODO: Write list to CSV
 
-	def parse(self):
-		"""Parse CSV object."""
-		self.csv.seek(0)
+		:param str path: Path to CSV file
+		:param list data: Decoded list to write on CSV file
+		"""
 
-		self.header = self.csv.readline().strip("\n\r").split("\t")
-		self.types = self.csv.readline().strip("\n\r").split("\t")
-
-		for line in self.csv:
-			line = line.rstrip("\n\r").replace("\\", "/").split("\t")
-
-			for col in range(len(line)):
-				if line[col] == "null":
-					line[col] = None
-				elif self.types[col] in ("BYTE", "WORD", "DWORD", "INT", "INT32", "INT64"):
-					line[col] = int(line[col])
-				elif self.types[col] in ("STRING", "DWORD_HEX"):
-					pass
-				else:
-					print("Unknown type: {0}".format(self.types[col]), file=sys.stderr)
-
-			self.rows.append(line)
-
-	def update(self, db, user=None, password=None):
 		pass
-
-	def write(self, path):
-		"""Write decoded CT to disk as a UTF-8 CSV file."""
-		self.csv.seek(0)
-		filename = "{0}/{1}.csv".format(path, self.name)
-		os.makedirs(os.path.dirname(filename), exist_ok=True)
-		with open(filename, "w", newline="\r\n") as copy:
-			copy.write(self.csv.read())
---
+++ asset/ct.py
@@ -0,0 +1,99 @@
+import os, sys, struct
+
+class CT(object):
+	"""
+	Gravity proprietary file format for Ragnarok Online 2 assets
+
+	TODO: Document file format
+	"""
+
+	name = ""
+	header = []
+	types = []
+	rows = []
+
+	def open(self, path):
+		"""
+		Parse CT file to a decoded list
+
+		:param str path: Path to CT file
+		:return: Decoded CT data
+		:rtype: list
+		"""
+
+		try:
+			self.ct = open(path, "rb")
+			self.ct.seek(64)
+		except (OSError, IOError):
+			print("could not open \"{0}\"".format(path), file=sys.stderr)
+
+		self.name = os.path.splitext(os.path.basename(path))[0]
+		self.header = [self.ct.read(2 * self._unpack()).decode("UTF-16LE") for i in range(self._unpack())]
+		self.types = [self._mstype(self._unpack()) for i in range(self._unpack())]
+		self.rows = [[self._unpack(t) for t in self.types] for i in range(self._unpack())]
+
+		self.ct.close()
+		return [self.header] + [self.types] + self.rows
+
+	def write(self, path, data):
+		"""
+		TODO: Write list to CT file
+
+		:param str path: Path to CT file
+		:param list data: Decoded list to write on CT file
+		"""
+
+		pass
+
+	@staticmethod
+	def _mstype(n):
+		"""
+		Decode MS-DTYP from CT type row
+
+		:param int n: Value from CT type row
+		:return: Windows Data Type
+		:rtype: str
+		"""
+
+		if n == 2: return "BYTE"
+		elif n == 3: return "SHORT"
+		elif n == 4: return "WORD"
+		elif n == 5: return "INT"
+		elif n == 6: return "DWORD"
+		elif n == 7: return "DWORD_HEX"
+		elif n == 8: return "STRING"
+		elif n == 9: return "FLOAT"
+		elif n == 11: return "INT64"
+		elif n == 12: return "BOOL"
+		else: print("No types for byte \"{0}\"".format(n), file=sys.stderr)
+
+	def _unpack(self, dtyp=None):
+		"""
+		Decode binary data from Windows data type
+
+		:param dtyp: MS-DTYP to decode, UINT32 if None (default)
+		:type dtyp: str or None
+		:return: byte(s) of requested type
+		"""
+
+		if dtyp in ("BYTE", "BOOL"):
+			return "{:d}".format(struct.unpack("B", self.ct.read(1))[0])
+		elif dtyp == "SHORT":
+			return "{:d}".format(struct.unpack("h", self.ct.read(2))[0])
+		elif dtyp == "WORD":
+			return "{:d}".format(struct.unpack("<H", self.ct.read(2))[0])
+		elif dtyp == "INT":
+			return "{:d}".format(struct.unpack("i", self.ct.read(4))[0])
+		elif dtyp == "DWORD":
+			return "{:d}".format(self._unpack())
+		elif dtyp == "DWORD_HEX":
+			return "0x{:08X}".format(self._unpack())
+		elif dtyp == "STRING":
+			length = self._unpack()
+			return self.ct.read(2 * length).decode("UTF-16LE") if length else "null"
+		elif dtyp == "FLOAT":
+			return "{:.6f}".format(struct.unpack("f", self.ct.read(4))[0])
+		elif dtyp == "INT64":
+			return "{:d}".format(struct.unpack("<Q", self.ct.read(8))[0])
+		else:
+			return struct.unpack("<L", self.ct.read(4))[0]
