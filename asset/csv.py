3915cbbe42aa0a3cb88129ab94388ba8bcd82adf
Gabriel Cavallo <gabrielcavallo@mail.com>
2016-07-30 01:41:25 -0400

    Rename CSV class to TSV

--- asset/csv.py
+++ asset/csv.py
@@ -2,48 +2,50 @@ import os
 
 from .asset import Asset
 
-class CSV(Asset):
+class TSV(Asset):
 	"""
-	Comma-separated values file format for Ragnarok Online 2 assets
+	Tab-separated values file format for Ragnarok Online 2 assets
 
-	:param str path: Path to CSV file
+	http://www.iana.org/assignments/media-types/text/tab-separated-values
+
+	:param str path: Path to TSV file
 	"""
 
 	def read(self):
 		"""
-		Parse CSV file to list
+		Parse TSV file to list
 
-		:return: Parsed CSV data
+		:return: Parsed TSV data
 		:rtype: list
 		"""
 
-		self.csv = open(self.path, "r", encoding="UTF-16", newline="\r\n")
+		self.tsv = open(self.path, "r", encoding="UTF-16", newline="\r\n")
 
-		self.header = self.csv.readline().rstrip("\r\n").split("\t")
-		self.types = self.csv.readline().rstrip("\r\n").split("\t")
-		self.rows = [line.rstrip("\r\n").split("\t") for line in self.csv]
+		self.header = self.tsv.readline().rstrip("\r\n").split("\t")
+		self.types = self.tsv.readline().rstrip("\r\n").split("\t")
+		self.rows = [line.rstrip("\r\n").split("\t") for line in self.tsv]
 
 		if self.verbose:
-			print("\nCSV read of \"{0}\" complete!\n".format(self.path))
+			print("\nTSV read of \"{0}\" complete!\n".format(self.path))
 
-		self.csv.close()
+		self.tsv.close()
 		return [self.header] + [self.types] + self.rows
 
 	def write(self, data):
 		"""
-		Write list to CSV
+		Write list to TSV
 
-		:param list data: Decoded list to write on CSV file
+		:param list data: Decoded list to write on TSV file
 		"""
 
 		os.makedirs(os.path.dirname("./{0}".format(self.path)), exist_ok=True)
-		self.csv = open(self.path, "w", encoding="UTF-16", newline="\r\n")
+		self.tsv = open(self.path, "w", encoding="UTF-16", newline="\r\n")
 
 		self.header = "\t".join(i for i in data[0]) + "\n"
 		self.types = "\t".join(i for i in data[1]) + "\n"
 		self.rows = "\n".join("\t".join(l) for l in data[2:]) + "\n"
 
-		self.csv.write(self.header + self.types + self.rows)
+		self.tsv.write(self.header + self.types + self.rows)
 
 		if self.verbose:
-			print("\nCSV write to \"{0}\" complete!\n".format(self.path))
+			print("\nTSV write to \"{0}\" complete!\n".format(self.path))
