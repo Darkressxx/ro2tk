d87caeeec21f6985659e9764c8bcba64fef27147
Darkressxx <Darkressxx1@gmail.com>
2023-01-26 11:56:58 -0800

    Update tsv.py
    reformat and optimization

--- asset/tsv.py
+++ asset/tsv.py
@@ -3,49 +3,63 @@ import os
 from .asset import Asset
 
 class TSV(Asset):
-	"""
-	Tab-separated values file format for Ragnarok Online 2 assets
-
-	http://www.iana.org/assignments/media-types/text/tab-separated-values
-
-	:param str path: Path to TSV file
-	"""
-
-	def read(self):
-		"""
-		Parse TSV file to list
-
-		:return: Parsed TSV data
-		:rtype: list
-		"""
-
-		self.tsv = open(self.path, "r", newline="\r\n")
-
-		self.header = self.tsv.readline().rstrip("\r\n").split("\t")
-		self.types = self.tsv.readline().rstrip("\r\n").split("\t")
-		self.rows = [line.rstrip("\r\n").split("\t") for line in self.tsv]
-
-		if self.verbose:
-			print("\nTSV read of \"{0}\" complete!\n".format(self.path))
-
-		self.tsv.close()
-		return [self.header] + [self.types] + self.rows
 
 	def write(self, data):
-		"""
-		Write list to TSV
+	    """
+	    Write list to TSV
+	    :param list data: Decoded list to write on TSV file
+	    """
+	    # Check if data is a list
+	    if not isinstance(data, list):
+	        print("Error: data must be a list. Please provide a valid list as the data parameter.")
+	        return
 
-		:param list data: Decoded list to write on TSV file
-		"""
+	    # Check if data has at least three elements (header, types, and at least one row)
+	    if len(data) < 3:
+	        print("Error: data must contain at least the header and types rows and one data row.")
+	        return
 
-		os.makedirs(os.path.dirname("./{0}".format(self.path)), exist_ok=True)
-		self.tsv = open(self.path, "w", newline="\r\n")
+	    try:
+	        os.makedirs(os.path.dirname("./{0}".format(self.path)), exist_ok=True)
+	        self.tsv = open(self.path, "w", encoding="UTF-16LE", newline="\r\n")
+	    except:
+	        print("Error: there was an issue opening the TSV file for writing.")
+	        return
 
-		self.header = "\t".join(i for i in data[0]) + "\n"
-		self.types = "\t".join(i for i in data[1]) + "\n"
-		self.rows = "\n".join("\t".join(l) for l in data[2:]) + "\n"
+	    self.header = "\t".join(i for i in data[0]) + "\n"
+	    self.types = "\t".join(i for i in data[1]) + "\n"
+	    self.rows = "\n".join("\t".join(str(cell) for cell in row) for row in data[2:]) + "\n"
 
-		self.tsv.write(self.header + self.types + self.rows)
 
-		if self.verbose:
-			print("\nTSV write to \"{0}\" complete!\n".format(self.path))
+	    self.tsv.write(self.header + self.types + self.rows)
+
+	    if self.verbose:
+	        print("\nTSV write to \"{0}\" complete!\n".format(self.path))
+
+
+	def read(self):
+	    """
+	    Parse TSV file to a decoded list
+
+	    :return: Decoded TSV data
+	    :rtype: list
+	    """
+	    try:
+	        self.tsv = open(self.path, "r", encoding="UTF-16LE")
+	    except FileNotFoundError:
+	        print("Error: the specified TSV file does not exist.")
+	        return
+	    except:
+	        print("Error: there was an issue opening the TSV file.")
+	        return
+
+	    self.header = self.tsv.readline().strip().split("\t")
+	    self.types = self.tsv.readline().strip().split("\t")
+	    self.rows = [l.strip().split("\t") for l in self.tsv.readlines()]
+
+	    if len(self.header) == 0 or len(self.types) == 0 or len(self.rows) == 0:
+	        print("Error: the TSV file is empty or has fewer than three rows.")
+	        return
+
+	    self.tsv.close()
+	    return [self.header, self.types] + self.rows
